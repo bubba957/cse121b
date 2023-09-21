ca8eb2d27055a3ac11072066d071007af4f041c9
bubba957 <scody49@gmail.com>
2023-09-20 19:32:47 -0700

    w02

---
+++ scripts/w02-task.js
@@ -0,0 +1,35 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+var fullName = `Cody Smith`;
+var currentYear = `2023`;
+var profilePicture = `temple.jpg`;
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById(`name`);
+const foodElement = document.getElementById(`food`);
+const yearElement = document.querySelector(`#year`);
+const imageElement = document.querySelector(`img`);
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentYear}`;
+imageElement.setAttribute(`src`, profilePicture);
+imageElement.setAttribute(`alt`, `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+
+foods = [`marshmallows`, `bacon and rice`, `chicken puffs`, `waffles`, `cereal`];
+foodElement.innerHTML = `${foods}`;
+var oneFood = `cupcakes`;
+foods.push(oneFood);
+foodElement.innerHTML += `<br>${foods}`;
+foods.shift();
+foodElement.innerHTML += `<br>${foods}`;
+foods.pop();
+foodElement.innerHTML += `<br>${foods}`;
